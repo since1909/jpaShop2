0177a6d1f231494e9c0cc16a5925b1418d323610
since1909 <since1909@daum.net>
2023-03-29 16:41:09 +0900

    Add : order and orderItem entity

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import org.aspectj.weaver.ast.Or;
+
 import static javax.persistence.FetchType.*;
 
 @Entity
@@ -26,4 +28,28 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
